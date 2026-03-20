ef58982dfcac1f3d0bc0d8c192e391cc283deaad
gtech29 <juan.rodriguez.517@my.csun.edu>
2026-03-20 09:45:24 -0700

    Add rental unit table creation to database.sql

--- database.sql
+++ database.sql
@@ -11,5 +11,16 @@ CREATE TABLE IF NOT EXISTS user (
     firstName VARCHAR(50) NOT NULL,
     lastName VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    phone VARCHAR(20) UNIQUE NOT NULL
+    phone VARCHAR(20) UNIQUE NOT NULL,
+);
+
+-- Phase 2: Create the rental unit table
+CREATE TABLE IF NOT EXISTS rental_unit (
+    rental_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    feature VARCHAR(50) NOT NULL,
+    price INT NOT NULL,
+    username VARCHAR(50),
+    FOREIGN KEY (username) REFERENCES user (username)
 );
